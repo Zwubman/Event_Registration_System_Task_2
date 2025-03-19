da27fb8c925f4489e57f4c5ff373c0b7bcc0ba2f
Zwubman <zwubeman@gmail.com>
2025-03-19 03:04:09 -0400

    DB connection

---
+++ DbConfigs/dbConfig.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbConfig = async() => {
+    try{
+        const response = await mongoose.connect(`${process.env.MONGO_URL}`);
+        if(response){
+            console.log("connected to DB");
+        }
+    }
+    catch(error){
+        console.error("connecting to DB" ,error.message);
+    }
+}
+
+dbConfig();
