8f69327e32db61fbc92596acbe537388264d4a31
Zwubman <zwubeman@gmail.com>
2025-03-20 02:42:27 -0400

    add some comment

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -4,6 +4,7 @@ import cookie from "cookie-parser";
 import bcrypt from "bcryptjs";
 import User from "../Models/userModel.js";
 
+// User sign up function
 export const signUp = async (req, res) => {
   try {
     const { firstName, lastName, email, password, role, phone } = req.body;
@@ -13,7 +14,7 @@ export const signUp = async (req, res) => {
     if (isExist) {
       res.status(400).json({ message: "User already exist" });
     }
-
+    //Create new user
     const user = new User({
       firstName: firstName,
       lastName: lastName,
@@ -30,6 +31,7 @@ export const signUp = async (req, res) => {
   }
 };
 
+// User sign in
 export const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -42,7 +44,7 @@ export const signIn = async (req, res) => {
     if (!isMatch) {
       res.status(401).json({ message: "Invalid credentail" });
     }
-
+    //Generate access token
     const accessToken = jwt.sign(
       {
         id: user._id,
@@ -52,13 +54,13 @@ export const signIn = async (req, res) => {
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "2h" }
     );
-
+    //Generate refresh token
     const refreshToken = jwt.sign(
       { email: email },
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: "30d" }
     );
-
+    //Store token in cookie
     res.cookie("accessToken", accessToken, { httpOnly: true });
     res.cookie("refreshToken", refreshToken, { httpOnly: true });
     res.json({ message: "Log In successfully.", accessToken, refreshToken });
